1106f5cd9cea481080daab516d0d25e274ff928a
Ksenoks8 <131990304+Ksenoks8@users.noreply.github.com>
2023-06-23 13:27:44 +0300

    Delete TG_gricenko directory

--- TG_gricenko/main.py
+++
@@ -1,9 +0,0 @@
-def main():
-    from core import bot
-    token = '5815502615:AAEJgn9HgFJNC2Yvo3p27xsgPdWM9NN44hY'
-    bot = bot.Bot(token)                              
-    bot.infinity_polling()
-
-
-if __name__ == '__main__':
-    main()
